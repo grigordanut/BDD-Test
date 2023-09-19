88d57898a07ec143096830536f90949a73bdff57
Grigor Danut <grigor@seamlesscare.ie>
2023-09-19 15:18:01 +0100

    Added Code to Given Step for Wales step definition

--- HomeAppliancesCost/StepDefinitions/WalesResidentCostStepDefinitions.cs
+++ HomeAppliancesCost/StepDefinitions/WalesResidentCostStepDefinitions.cs
@@ -91,7 +91,15 @@ namespace HomeAppliancesCost.StepDefinitions
         [Then(@"I should get the results table with daily, weekly, monthly, and yearly costs is")]
         public void ThenIShouldGetTheResultsTableWithDailyWeeklyMonthlyAndYearlyCostsIs()
         {
-            throw new PendingStepException();
+            string actual = driver.FindElement(By.XPath("//*[@id=\"RootPlaceHolder_RootPlaceHolder_SubHeading\"]/span")).Text;
+            Console.WriteLine(actual);
+            String expected = "This advice applies to Wales";
+            Assert.AreEqual(actual, expected);
+            driver.FindElement(By.XPath("//*[@id=\"appliance_running\"]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//*[@id=\"reset\"]")).Click();
+            driver.SwitchTo().Alert().Accept();
+            driver.Quit()
         }
     }
 }
