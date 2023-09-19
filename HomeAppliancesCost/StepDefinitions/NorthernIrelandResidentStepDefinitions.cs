d232f637c82dc2e4942226534d9bf52accedeb57
Grigor Danut <grigor@seamlesscare.ie>
2023-09-19 15:27:14 +0100

    Added Northern Ireland Step definition

---
+++ HomeAppliancesCost/StepDefinitions/NorthernIrelandResidentStepDefinitions.cs
@@ -0,0 +1,27 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace HomeAppliancesCost.StepDefinitions
+{
+    [Binding]
+    public class NorthernIrelandResidentStepDefinitions
+    {
+        [Given(@"I am a resident from Northern Ireland")]
+        public void GivenIAmAResidentFromNorthernIreland()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I select This advice applies to Northern Ireland")]
+        public void WhenISelectThisAdviceAppliesToNorthernIreland()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should get the results message as ‘The advice on this website doesn’t cover Northern Ireland’")]
+        public void ThenIShouldGetTheResultsMessageAsTheAdviceOnThisWebsiteDoesnTCoverNorthernIreland()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
