b632adc41ace347f5c5569ad3557256ac07c117d
Grigor Danut <grigor@seamlesscare.ie>
2023-09-19 06:18:09 +0100

    Added the England Step definition

---
+++ HomeAppliancesCost/StepDefinitions/EnglandResidentCostStepDefinitions.cs
@@ -0,0 +1,27 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace HomeAppliancesCost.StepDefinitions
+{
+    [Binding]
+    public class EnglandResidentCostStepDefinitions
+    {
+        [Given(@"I am a resident from England")]
+        public void GivenIAmAResidentFromEngland()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I add the list appliances Electric blanket and its average usage and the national average rate")]
+        public void WhenIAddTheListAppliancesElectricBlanketAndItsAverageUsageAndTheNationalAverageRate()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should get the results table with daily, weekly, monthly, and yearly cost")]
+        public void ThenIShouldGetTheResultsTableWithDailyWeeklyMonthlyAndYearlyCost()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
