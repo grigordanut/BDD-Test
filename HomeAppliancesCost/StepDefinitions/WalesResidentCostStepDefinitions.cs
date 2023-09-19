74395dd2f6a4ca92aa69f7e9df4591ec78d58fb6
Grigor Danut <grigor@seamlesscare.ie>
2023-09-19 10:59:33 +0100

    Added Wales Step definition

---
+++ HomeAppliancesCost/StepDefinitions/WalesResidentCostStepDefinitions.cs
@@ -0,0 +1,27 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace HomeAppliancesCost.StepDefinitions
+{
+    [Binding]
+    public class WalesResidentCostStepDefinitions
+    {
+        [Given(@"I am a resident from Wales")]
+        public void GivenIAmAResidentFromWales()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I add the list appliance Immersion heater and its average usage and the national average rates")]
+        public void WhenIAddTheListApplianceImmersionHeaterAndItsAverageUsageAndTheNationalAverageRates()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should get the results table with daily, weekly, monthly, and yearly costs is")]
+        public void ThenIShouldGetTheResultsTableWithDailyWeeklyMonthlyAndYearlyCostsIs()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
