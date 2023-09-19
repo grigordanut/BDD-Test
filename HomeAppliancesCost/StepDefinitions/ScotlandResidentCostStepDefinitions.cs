8308e422b37a9baea0625d803c32466360ae2931
Grigor Danut <grigor@seamlesscare.ie>
2023-09-19 08:41:54 +0100

    Added code to open the Citizen Advice website

--- HomeAppliancesCost/StepDefinitions/ScotlandResidentCostStepDefinitions.cs
+++ HomeAppliancesCost/StepDefinitions/ScotlandResidentCostStepDefinitions.cs
@@ -14,7 +14,12 @@ namespace HomeAppliancesCost.StepDefinitions
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            
+            driver.Navigate().GoToUrl("https://www.citizensadvice.org.uk/");
+            driver.FindElement(By.XPath("//*[@id=\"home-extent-popup\"]/div/div/a[3]")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//*[@id=\"main-nav\"]/ul/li[4]/a")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"main\"]/div[2]/div/div[2]/ul/li/a")).Click();
+            driver.FindElement(By.XPath("//*[@id=\"main\"]/div[2]/div/div[2]/ul/li[6]/a")).Click();
         }
 
         [When(@"I add the list appliances Electric blanket and its average usage and the national average rates")]
